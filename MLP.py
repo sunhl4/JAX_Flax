300b13c4f840568d086e7c1886ae504540939d91
sunhl4 <15051962326@163.com>
2023-09-21 23:04:04 +0800

    new

--- MLP.py
+++ MLP.py
@@ -2,7 +2,7 @@ import pandas as pd
 from flax import linen as nn  # Linen API
 import jax
 import jax.numpy as jnp
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from clu import metrics
 from flax.training import train_state  # Useful dataclass to keep train state
 from flax import struct                # Flax dataclasses
@@ -10,25 +10,31 @@ import optax
 import tensorflow_datasets as tfds  # TFDS for MNIST
 import tensorflow as tf             # TensorFlow operations
 # Common loss functions and optimizers
-data = pd.DataFrame('./average.csv')
+data = pd.read_csv('./average.csv')
+x = jnp.array(data['frame'])
+y = jnp.array(data['1300K'])
+x = x.reshape(-1,1)
+y = y.reshape(-1,1)
 class MLP(nn.Module):                    # create a Flax Module dataclass
   out_dims: int
 
   @nn.compact
   def __call__(self, x):
-    x = x.reshape((1, -1))
-    x = nn.Dense(128)(x)                 # create inline Flax Module submodules
+    x = x.reshape((-1, 1))
+    x = nn.Dense(10)(x)                 # create inline Flax Module submodules
+    x = nn.relu(x)
+    x = nn.Dense(10)(x)
     x = nn.relu(x)
     x = nn.Dense(self.out_dims)(x)       # shape inference
     return x
 
-model = MLP(out_dims=10)                 # instantiate the MLP model
-key = jax.random.PRNGKey(17)
-x = jnp.empty((4, 28, 28, 1))            # generate random data
+model = MLP(out_dims=1)                 # instantiate the MLP model
+key = jax.random.PRNGKey(17)           # generate random data
 variables = model.init(jax.random.PRNGKey(42), x)# initialize the weights
-y = model.apply(variables, x)
+y_pred = model.apply(variables, x)
+print(jnp.shape(y_pred))
 plt.plot(x, y, "r-", label="real")
-
+plt.plot(x, y_pred, 'b', label = 'pred')
 plt.legend(loc="upper left")
 plt.grid(True)
 plt.show()
---
+++ test.py
@@ -0,0 +1,64 @@
+import jax
+from typing import Any, Callable, Sequence
+import jax.numpy as jnp
+from jax import random
+import flax
+from flax import linen as nn
+
+# We create one dense layer instance (taking 'features' parameter as input)
+model = nn.Dense(features=5)
+# key = random.PRNGKey(17)
+key1, key2 = random.split(random.PRNGKey(17))
+
+x = random.normal(key1, (10,)) # Dummy input data
+params = model.init(key2, x) # Initialization call
+print(jax.tree_util.tree_map(lambda x: x.shape, params)) # Checking output shapes
+model.apply(params, x)# 模型每次执行前向传播时需要使用apply方法传参数
+# Set problem dimensions.
+n_samples = 20
+x_dim = 10
+y_dim = 5
+
+# Generate random ground truth W and b.
+key = random.PRNGKey(42)
+k1, k2 = random.split(key)
+# print(k1)
+W = random.normal(k1, (x_dim, y_dim))
+b = random.normal(k2, (y_dim,))
+# Store the parameters in a FrozenDict pytree.
+#通常，在定义模型和训练之前，会使用类似的方式创建和冻结参数 避免参数在训练过程被修改保持稳定性
+true_params = flax.core.freeze({'params': {'bias': b, 'kernel': W}})
+
+# Generate samples with additional noise.
+key_sample, key_noise = random.split(k1)
+x_samples = random.normal(key_sample, (n_samples, x_dim))
+y_samples = jnp.dot(x_samples, W) + b + 0.1 * random.normal(key_noise,(n_samples, y_dim))
+print('x shape:', x_samples.shape, '; y shape:', y_samples.shape)
+
+# Same as JAX version but using model.apply().
+@jax.jit
+def mse(params, x_batched, y_batched):
+  # Define the squared loss for a single pair (x,y)
+    def squared_error(x, y):
+        pred = model.apply(params, x)
+        #inner计算了误差向量的内积（点积），也就是两个向量的逐元素乘积之和。这个操作得到了误差向量的平方和
+        return jnp.inner(y-pred, y-pred) / 2.0
+  # Vectorize the previous to compute the average of the loss on all samples.
+    return jnp.mean(jax.vmap(squared_error)(x_batched,y_batched), axis=0)
+
+learning_rate = 0.3  # Gradient step size.
+print('Loss for "true" W,b: ', mse(true_params, x_samples, y_samples))
+loss_grad_fn = jax.value_and_grad(mse)
+
+@jax.jit
+def update_params(params, learning_rate, grads):
+    params = jax.tree_util.tree_map(
+        lambda p, g: p - learning_rate * g, params, grads)
+    return params
+
+for i in range(101):
+    # Perform one gradient update.
+    loss_val, grads = loss_grad_fn(params, x_samples, y_samples)
+    params = update_params(params, learning_rate, grads)
+    if i % 10 == 0:
+        print(f'Loss step {i}: ', loss_val)
